ba5b8f11554d40b3546fbd31e7416386870f2928
Ahmed Jihad <ahmadjihadoffical@gmail.com>
2025-03-05 11:05:11 +0300

    Add galary in accounting solutions

---
+++ src/Pages/Accounting Solutions/images.js
@@ -0,0 +1,113 @@
+import img1 from "../../assets/accounting sloutions/imgSlider/photo_1_2025-03-05_01-58-27.jpg";
+import img2 from "../../assets/accounting sloutions/imgSlider/photo_2_2025-03-05_01-58-27.jpg";
+import img3 from "../../assets/accounting sloutions/imgSlider/photo_3_2025-03-05_01-58-27.jpg";
+import img4 from "../../assets/accounting sloutions/imgSlider/photo_4_2025-03-05_01-58-27.jpg";
+import img5 from "../../assets/accounting sloutions/imgSlider/photo_5_2025-03-05_01-58-27.jpg";
+import img6 from "../../assets/accounting sloutions/imgSlider/photo_6_2025-03-05_01-58-27.jpg";
+import img7 from "../../assets/accounting sloutions/imgSlider/photo_7_2025-03-05_01-58-27.jpg";
+import img8 from "../../assets/accounting sloutions/imgSlider/photo_8_2025-03-05_01-58-27.jpg";
+import img9 from "../../assets/accounting sloutions/imgSlider/photo_9_2025-03-05_01-58-27.jpg";
+import img10 from "../../assets/accounting sloutions/imgSlider/photo_10_2025-03-05_01-58-27.jpg";
+
+import img11 from "../../assets/accounting sloutions/imgSlider/photo_11_2025-03-05_01-58-27.jpg";
+import img12 from "../../assets/accounting sloutions/imgSlider/photo_12_2025-03-05_01-58-27.jpg";
+import img13 from "../../assets/accounting sloutions/imgSlider/photo_13_2025-03-05_01-58-27.jpg";
+import img14 from "../../assets/accounting sloutions/imgSlider/photo_14_2025-03-05_01-58-27.jpg";
+import img15 from "../../assets/accounting sloutions/imgSlider/photo_15_2025-03-05_01-58-27.jpg";
+import img16 from "../../assets/accounting sloutions/imgSlider/photo_16_2025-03-05_01-58-27.jpg";
+import img17 from "../../assets/accounting sloutions/imgSlider/photo_17_2025-03-05_01-58-27.jpg";
+import img18 from "../../assets/accounting sloutions/imgSlider/photo_18_2025-03-05_01-58-27.jpg";
+import img19 from "../../assets/accounting sloutions/imgSlider/photo_19_2025-03-05_01-58-27.jpg";
+import img20 from "../../assets/accounting sloutions/imgSlider/photo_20_2025-03-05_01-58-27.jpg";
+
+import img21 from "../../assets/accounting sloutions/imgSlider/photo_21_2025-03-05_01-58-27.jpg";
+import img22 from "../../assets/accounting sloutions/imgSlider/photo_22_2025-03-05_01-58-27.jpg";
+import img23 from "../../assets/accounting sloutions/imgSlider/photo_23_2025-03-05_01-58-27.jpg";
+import img24 from "../../assets/accounting sloutions/imgSlider/photo_24_2025-03-05_01-58-27.jpg";
+import img25 from "../../assets/accounting sloutions/imgSlider/photo_25_2025-03-05_01-58-27.jpg";
+import img26 from "../../assets/accounting sloutions/imgSlider/photo_26_2025-03-05_01-58-27.jpg";
+import img27 from "../../assets/accounting sloutions/imgSlider/photo_27_2025-03-05_01-58-27.jpg";
+import img28 from "../../assets/accounting sloutions/imgSlider/photo_28_2025-03-05_01-58-27.jpg";
+import img29 from "../../assets/accounting sloutions/imgSlider/photo_29_2025-03-05_01-58-27.jpg";
+import img30 from "../../assets/accounting sloutions/imgSlider/photo_30_2025-03-05_01-58-27.jpg";
+
+import img31 from "../../assets/accounting sloutions/imgSlider/photo_31_2025-03-05_01-58-27.jpg";
+import img32 from "../../assets/accounting sloutions/imgSlider/photo_32_2025-03-05_01-58-27.jpg";
+import img33 from "../../assets/accounting sloutions/imgSlider/photo_33_2025-03-05_01-58-27.jpg";
+import img34 from "../../assets/accounting sloutions/imgSlider/photo_34_2025-03-05_01-58-27.jpg";
+import img35 from "../../assets/accounting sloutions/imgSlider/photo_35_2025-03-05_01-58-27.jpg";
+import img36 from "../../assets/accounting sloutions/imgSlider/photo_36_2025-03-05_01-58-27.jpg";
+import img37 from "../../assets/accounting sloutions/imgSlider/photo_37_2025-03-05_01-58-27.jpg";
+import img38 from "../../assets/accounting sloutions/imgSlider/photo_38_2025-03-05_01-58-27.jpg";
+import img39 from "../../assets/accounting sloutions/imgSlider/photo_39_2025-03-05_01-58-27.jpg";
+import img40 from "../../assets/accounting sloutions/imgSlider/photo_40_2025-03-05_01-58-27.jpg";
+
+import img41 from "../../assets/accounting sloutions/imgSlider/photo_41_2025-03-05_01-58-27.jpg";
+import img42 from "../../assets/accounting sloutions/imgSlider/photo_42_2025-03-05_01-58-27.jpg";
+import img43 from "../../assets/accounting sloutions/imgSlider/photo_43_2025-03-05_01-58-27.jpg";
+import img44 from "../../assets/accounting sloutions/imgSlider/photo_44_2025-03-05_01-58-27.jpg";
+import img45 from "../../assets/accounting sloutions/imgSlider/photo_45_2025-03-05_01-58-27.jpg";
+import img46 from "../../assets/accounting sloutions/imgSlider/photo_46_2025-03-05_01-58-27.jpg";
+import img47 from "../../assets/accounting sloutions/imgSlider/photo_47_2025-03-05_01-58-27.jpg";
+import img48 from "../../assets/accounting sloutions/imgSlider/photo_48_2025-03-05_01-58-27.jpg";
+import img49 from "../../assets/accounting sloutions/imgSlider/photo_49_2025-03-05_01-58-27.jpg";
+
+export const images1 = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+];
+export const images2 = [
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+];
+export const images3 = [
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+  img27,
+  img28,
+  img29,
+  img30,
+];
+export const images4 = [
+  img31,
+  img32,
+  img33,
+  img34,
+  img35,
+  img36,
+  img37,
+  img38,
+  img39,
+  img40,
+];
+export const images5 = [
+  img41,
+  img42,
+  img43,
+  img44,
+  img45,
+  img46,
+  img47,
+  img48,
+  img49,
+];
