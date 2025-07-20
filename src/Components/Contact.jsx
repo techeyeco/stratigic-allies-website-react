5ed2e0f7ad758cc1728e6ffaaad8f0ba6e48cb10
Ahmed Jihad <ahmadjihadoffical@gmail.com>
2025-07-20 12:37:50 +0300

    Fix the git issue

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63f500e4d4cd57c1d3ef5ebfe2ccbde13c3d88afd92875e250f1c99f4afa025e
-size 13135
+oid sha256:bfc311b35eea7bb67dcf62e0b2540d10fab7ec61adb4aab8a95a7f45607f0f1e
+size 13114
