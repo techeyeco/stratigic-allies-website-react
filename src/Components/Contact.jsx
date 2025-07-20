0ebfce05a5af43e66d2c7c591580142661102780
Ahmed Jihad <ahmadjihadoffical@gmail.com>
2025-07-20 12:36:20 +0300

    Add back the contact us

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a805ba4e4b8808678e0a145dc141e01a26f95c638eb45cf569dee664865b90b
-size 13128
+oid sha256:63f500e4d4cd57c1d3ef5ebfe2ccbde13c3d88afd92875e250f1c99f4afa025e
+size 13135
